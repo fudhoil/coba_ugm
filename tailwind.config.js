b0d8680522f9ae43edc752aff0fa29af7d664dce
muhammadbintangkh <muhammad.bintang.khatulistiwa@hashmicro.com>
2022-09-30 09:01:17 +0700

    add register and submit form

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,17 @@
 module.exports = {
-  content: ['index.html'],
+  content: ["./public/**/*.{html,js}", "index.html","cew.html","academic_forum","about.html","register.html","submition.html"],
   theme: {
     container:{
       center: true,
       padding: '16px',
     },
     extend: {
+      fontFamily:{
+        Lora: "'Lora', serif",
+        Roboto: ['Roboto', 'sans-serif'],
+        Montserrat: ['Montserrat', 'sans-serif'],
+        Poppins: ['Poppins', 'sans-serif'],
+      },
       colors: {
         primary: '#0ea5e9',
         dark: '#1e293b',
